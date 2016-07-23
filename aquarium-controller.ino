1552027b82ceaedf27d5545bfc23125fca4bb7aa
Charles Wilkinson <charles.wilkinson@kedos.co.uk>
2016-07-23 18:18:36 +0100

    Removed need for interrupts as this was causing a glitchy display. Other organisational tweaks.

--- aquarium-controller.ino
+++ aquarium-controller.ino
@@ -1,7 +1,6 @@
 #include <LiquidCrystal.h> // https://www.arduino.cc/en/Reference/LiquidCrystal
 #include <DS1302RTC.h>     // http://playground.arduino.cc/Main/DS1302RTC
 #include <Time.h>          // http://playground.arduino.cc/Code/Time
-#include <TimerOne.h>      // http://playground.arduino.cc/Code/Timer1
 
 // Setup RTC pins: 
 //            CE, IO, CLK
@@ -25,6 +24,8 @@ const int CO2_ON_MINUTE = 0;
 const int CO2_OFF_HOUR = 21;
 const int CO2_OFF_MINUTE = 30;
 
+String CO2_STATE = "OFF";
+
 const int LIGHTS_ON_HOUR = 8;
 const int LIGHTS_ON_MINUTE = 0;
 const int LIGHTS_OFF_HOUR = 22;
@@ -38,6 +39,8 @@ const long MILLIS_PER_INCREMENT = 2300; // Roughly 10 minutes
 int currentBrightness = 0;
 
 unsigned long previousMillis = 0;
+
+bool INITIALISED_SUCCESS = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -62,31 +65,28 @@ void setup() {
   if (RTC.haltRTC()) {
     lcd.print("Please set time!");
   } else {
-    Timer1.initialize(1000000);
-    Timer1.attachInterrupt(updateDisplay); // update lcd display every 1 second
+    initialiseDisplay();
+    INITIALISED_SUCCESS = true;
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  handleCo2();
-  handleLights();
-
-  // We have to have some small delay each cycle in order to allow the interrupt to execute.
-  delay(500);
+  if (INITIALISED_SUCCESS) {
+    updateDisplay();
+    handleCo2();
+    handleLights();
+  }
 }
 
 // Turn the CO2 on or off
 void co2On(bool on) {
   if (on) {
     digitalWrite(SOLENOID_PIN, HIGH);
-    lcd.setCursor(0, 2);
-    lcd.print("CO2: ON         ");
+    CO2_STATE = "ON ";
   } else {
     digitalWrite(SOLENOID_PIN, LOW);
-    lcd.setCursor(0, 2);
-    lcd.print("CO2: OFF        ");
+    CO2_STATE = "OFF";
   }
 }
 
@@ -107,7 +107,7 @@ void handleLights() {
 
   if (currentMillis - previousMillis >= MILLIS_PER_INCREMENT) {
     previousMillis = currentMillis;
-  
+    
     int currentHour = hour();
     int currentMin = minute();
     if ((currentHour > LIGHTS_ON_HOUR || (currentHour == LIGHTS_ON_HOUR && currentMin >= LIGHTS_ON_MINUTE))
@@ -128,31 +128,30 @@ void handleLights() {
 
 // Prints the date & time to the display
 void updateDisplay() {
-  lcd.setCursor(0, 0);
-  lcd.print("Date: ");
+  lcd.setCursor(6, 0);
   printTwoDigits(day());
   lcd.print("/");
   printTwoDigits(month());
   lcd.print("/");
   lcd.print(year());
 
-  lcd.setCursor(0, 1);
-  lcd.print("Time: ");
+  lcd.setCursor(6, 1);
   printTwoDigits(hour());
   lcd.print(":");
   printTwoDigits(minute());
   lcd.print(":");
   printTwoDigits(second());
 
-  lcd.setCursor(0, 3);
+  lcd.setCursor(5, 2);
+  lcd.print(CO2_STATE);
+
+  lcd.setCursor(8, 3);
   if (currentBrightness == 255) {
-    lcd.print("Lights: ON      ");
+    lcd.print("ON ");
   } else if (currentBrightness == 0) {
-    lcd.print("Lights: OFF     ");
+    lcd.print("OFF");
   } else {
-    lcd.print("Lights: ");
-    lcd.print(currentBrightness);
-    lcd.print("     ");
+    printBrightness(currentBrightness);
   }
 }
 
@@ -163,3 +162,26 @@ void printTwoDigits(int number) {
   }
   lcd.print(number);
 }
+
+// Suffixes with spaces to clear trailing characters on the display.
+void printBrightness(int brightness) {
+  lcd.print(brightness);
+  if (brightness >= 0 && brightness < 10) {
+    lcd.print("  ");
+  } else if (brightness < 100) {
+    lcd.print(" ");
+  }
+}
+
+void initialiseDisplay() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Date: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Time: ");
+  lcd.setCursor(0, 2);
+  lcd.print("CO2: ");
+  lcd.setCursor(0, 3);
+  lcd.print("Lights: ");
+}
+
