2af97c79a948f4895ff173d4c5003d76c6028e69
Charles Wilkinson <charles.wilkinson@kedos.co.uk>
2016-07-23 17:25:43 +0100

    Set the time for CO2 and Lights on and off. Added ramp up/down for the lights

--- aquarium-controller.ino
+++ aquarium-controller.ino
@@ -20,6 +20,25 @@ LiquidCrystal lcd(6, 7, 0, 1, 2, 3);
 const int LIGHTS_PWM_PIN = 5;
 const int SOLENOID_PIN = 8;
 
+const int CO2_ON_HOUR = 7;
+const int CO2_ON_MINUTE = 0;
+const int CO2_OFF_HOUR = 21;
+const int CO2_OFF_MINUTE = 30;
+
+const int LIGHTS_ON_HOUR = 8;
+const int LIGHTS_ON_MINUTE = 0;
+const int LIGHTS_OFF_HOUR = 22;
+const int LIGHTS_OFF_MINUTE = 30;
+
+// This is the number of milliseconds between each increment of brightness when ramping
+// E.g a value of 2000 means 2x255 = 8.5 minutes of ramp
+const long MILLIS_PER_INCREMENT = 2300; // Roughly 10 minutes
+
+// Max brightness is 255
+int currentBrightness = 0;
+
+unsigned long previousMillis = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -44,55 +63,71 @@ void setup() {
     lcd.print("Please set time!");
   } else {
     Timer1.initialize(1000000);
-    Timer1.attachInterrupt(displayDateTime); // update time & date every 0.5 seconds
+    Timer1.attachInterrupt(updateDisplay); // update lcd display every 1 second
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(SOLENOID_PIN, LOW);
-  lcd.setCursor(0, 2);
-  lcd.print("CO2: OFF        ");
+  handleCo2();
+  handleLights();
 
-  int brightness = 0;
-  while(brightness < 255) {
-    brightness++;
-    analogWrite(LIGHTS_PWM_PIN, brightness);
-    delay(10);
-  }
-
-  lcd.setCursor(0, 3);
-  lcd.print("LIGHTS: ON      ");
-
-  delay(2000);
-
-  digitalWrite(SOLENOID_PIN, HIGH);
-  lcd.setCursor(0, 2);
-  lcd.print("CO2: ON         ");
-
-  while(brightness > 0) {
-    brightness--;
-    analogWrite(LIGHTS_PWM_PIN, brightness);
-    delay(10);
-  }
-
-  analogWrite(LIGHTS_PWM_PIN, 0);
-
-  lcd.setCursor(0, 3);
-  lcd.print("LIGHTS: OFF     ");
-  delay(2000);
+  // We have to have some small delay each cycle in order to allow the interrupt to execute.
+  delay(500);
 }
 
-// Prefixes single digits with 0
-void printTwoDigits(int number) {
-  if (number >= 0 && number < 10) {
-    lcd.print("0");
+// Turn the CO2 on or off
+void co2On(bool on) {
+  if (on) {
+    digitalWrite(SOLENOID_PIN, HIGH);
+    lcd.setCursor(0, 2);
+    lcd.print("CO2: ON         ");
+  } else {
+    digitalWrite(SOLENOID_PIN, LOW);
+    lcd.setCursor(0, 2);
+    lcd.print("CO2: OFF        ");
   }
-  lcd.print(number);
 }
 
-void displayDateTime() {
+void handleCo2() {
+  int currentHour = hour();
+    int currentMin = minute();
+    if ((currentHour > CO2_ON_HOUR || (currentHour == CO2_ON_HOUR && currentMin >= CO2_ON_MINUTE))
+        && (currentHour < CO2_OFF_HOUR || (currentHour == CO2_OFF_HOUR && currentMin < CO2_OFF_MINUTE))
+    ) {
+      co2On(true);
+    } else {
+      co2On(false);
+    }
+}
+
+void handleLights() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= MILLIS_PER_INCREMENT) {
+    previousMillis = currentMillis;
+  
+    int currentHour = hour();
+    int currentMin = minute();
+    if ((currentHour > LIGHTS_ON_HOUR || (currentHour == LIGHTS_ON_HOUR && currentMin >= LIGHTS_ON_MINUTE))
+        && (currentHour < LIGHTS_OFF_HOUR || (currentHour == LIGHTS_OFF_HOUR && currentMin < LIGHTS_OFF_MINUTE))
+    ) {
+      if (currentBrightness < 255) {
+        currentBrightness++;
+        analogWrite(LIGHTS_PWM_PIN, currentBrightness);
+      }
+    } else {
+      if (currentBrightness > 0) {
+        currentBrightness--;
+        analogWrite(LIGHTS_PWM_PIN, currentBrightness);
+      }
+    }
+  }
+}
+
+// Prints the date & time to the display
+void updateDisplay() {
   lcd.setCursor(0, 0);
   lcd.print("Date: ");
   printTwoDigits(day());
@@ -108,4 +143,23 @@ void displayDateTime() {
   printTwoDigits(minute());
   lcd.print(":");
   printTwoDigits(second());
+
+  lcd.setCursor(0, 3);
+  if (currentBrightness == 255) {
+    lcd.print("Lights: ON      ");
+  } else if (currentBrightness == 0) {
+    lcd.print("Lights: OFF     ");
+  } else {
+    lcd.print("Lights: ");
+    lcd.print(currentBrightness);
+    lcd.print("     ");
+  }
+}
+
+// Prefixes single digits with 0
+void printTwoDigits(int number) {
+  if (number >= 0 && number < 10) {
+    lcd.print("0");
+  }
+  lcd.print(number);
 }
